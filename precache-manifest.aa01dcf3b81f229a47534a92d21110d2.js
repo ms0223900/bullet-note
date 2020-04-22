e70d5e653a839f4a6032ab7334e2eae3f1118e91
ms0223900 <ms0223900@gmail.com>
2020-04-22 15:45:26 +0800

    Updates

---
+++ precache-manifest.aa01dcf3b81f229a47534a92d21110d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976b42fe89e65cd0418b3706bff4dcc4",
+    "url": "/bullet-note/index.html"
+  },
+  {
+    "revision": "d34123551026eb3f2f1b",
+    "url": "/bullet-note/static/css/main.0d172d4a.chunk.css"
+  },
+  {
+    "revision": "608c93fe2a99bfb6a574",
+    "url": "/bullet-note/static/js/2.872f84e3.chunk.js"
+  },
+  {
+    "revision": "a8382be8c3f6b499838384d253e88a50",
+    "url": "/bullet-note/static/js/2.872f84e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34123551026eb3f2f1b",
+    "url": "/bullet-note/static/js/main.2720ceca.chunk.js"
+  },
+  {
+    "revision": "d181fa10d5779ef9d4e4",
+    "url": "/bullet-note/static/js/runtime-main.1773b4ad.js"
+  }
+]);
